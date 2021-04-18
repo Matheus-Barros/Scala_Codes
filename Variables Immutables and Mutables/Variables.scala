020c0d218aee68568bdbcbcb63eb2565611dcd73
Matheus Barros <matheuspopx@gmail.com>
2021-04-18 13:48:10 -0300

    Scala

---
+++ Variables Immutables and Mutables/Variables.scala
@@ -0,0 +1,18 @@
+//@Author: Matheus Barros
+//Date: 03/18/2021
+
+//DELACRING MUTABLES VARIABLES
+
+var Variable1 = 10
+//OR
+var Variable2: String = "Test"
+
+println(Variable1)
+
+//DELACRING IMMUTABLES VARIABLES
+
+val Variable3: Int = 10
+//OR
+val Variable4 = "Test"
+
+println(Variable4)
